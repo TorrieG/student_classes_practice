18a97080b378ab85e419e3378168f52da8b3437c
TorrieG <102880414+TorrieG@users.noreply.github.com>
2022-05-09 09:48:35 -0700

    first commit

--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,15 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
 
         //COMPLETE THIS CLASS AFTER ALL THE OTHER CLASSES
 
         /*
-        Write a code that asks user to if they would like join your classes
+        Write a code that asks user if they would like to join your classes
         If the answer is "Yes" from user,then ask user information; firstName, lastName, age, gender, class to join
-        ***If user is age is not more than 20, do not allow them to join
+        ***If user age is not more than 20, do not allow them to join
         ***If user wants to join any other class except Math and Science, do not allow them to join
 
         REMEMBER - checkAge and checkClassName methods may throw exceptions. You have to handle them
@@ -22,5 +25,63 @@ public class Test {
         Print how many students are MathStudent with message -> "Math students = {numberOfMathStudents}"
         Print how many students are ScienceStudent with message -> "Science students = {numberOfScienceStudents}"
          */
+        Scanner input = new Scanner(System.in);
+        int studentCount = 0, math = 0, science = 0;
+        ArrayList<Student> students = new ArrayList<>();
+
+        do {
+            System.out.println(UserQuestions.askToJoin);
+            String join = input.next();
+
+            if (join.toLowerCase().startsWith("y")) {
+
+                System.out.println(UserQuestions.askFirstName);
+                String firstName = input.next();
+
+                System.out.println(UserQuestions.askLastName);
+                String lastName = input.next();
+
+                System.out.println(UserQuestions.askAge);
+                int age = input.nextInt();
+                try {
+                    Permission.checkAge(age);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                if (age > 20) {
+                    System.out.println(UserQuestions.askGender);
+                    String gender = input.next();
+
+                    System.out.println(UserQuestions.askClassName);
+                    String className = input.next();
+                    try {
+                        Permission.checkClassName(className);
+                        if (className.equalsIgnoreCase("math")) {
+                            studentCount++;
+                            math++;
+                            students.add(new MathStudent(firstName, lastName, age, gender, className));
+
+                            System.out.println("Congratulations! You are registered for Math class.\n");
+                        } else if (className.equalsIgnoreCase("science")) {
+                            studentCount++;
+                            science++;
+                            students.add(new ScienceStudent(firstName, lastName, age, gender, className));
+
+                            System.out.println("Congratulations! You are registered for Science class.\n");
+                        }
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+        } while (studentCount < 3);
+
+        for (Student i : students) {
+            System.out.println(i);
+        }
+        System.out.println("\nMath students = " + math);
+        System.out.println("Science students = " + science);
     }
 }
